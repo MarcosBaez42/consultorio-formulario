d66cbddf047dfae02463ad98ea597e62aff8a312
Marcos Baez <marcosa_baez@soy.sena.edu.co>
2025-09-08 11:46:13 -0500

    arreglo de rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,14 @@
+import MainLayout from '../layouts/MainLayout.vue'
 import MedicalForm from '../pages/MedicalForm.vue'
 
 const routes = [
-  { path: '/', component: MedicalForm }
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      { path: '', component: MedicalForm }
+    ]
+  }
 ]
 
 export default routes
