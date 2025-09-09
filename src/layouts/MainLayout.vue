fd2b0a27a5d0e2ae78616ff73bb0d07d271c72d1
Marcos Baez <marcosa_baez@soy.sena.edu.co>
2025-09-09 10:01:16 -0500

    eliminacion de menu lateral

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,55 +2,14 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
           Gestión de Pacientes
         </q-toolbar-title>
-        <div>v1.0</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label header>
-          Menú Principal
-        </q-item-label>
-
-        <q-item clickable to="/">
-          <q-item-section avatar>
-            <q-icon name="home" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Inicio</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable to="/registro">
-          <q-item-section avatar>
-            <q-icon name="person_add" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Nuevo Paciente</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item clickable to="/pacientes">
-          <q-item-section avatar>
-            <q-icon name="assignment_ind" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Pacientes Registrados</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
